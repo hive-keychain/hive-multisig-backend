43bc614e875e4c7814775b6b7447da41a7683b43
Tying Shiuan <t.shiuan@gmail.com>
2023-08-14 20:03:06 +0800

    feat(signatureRequest): Adding createdAt and updatedAt

--- src/signature-request/signature-request.entity.ts
+++ src/signature-request/signature-request.entity.ts
@@ -1,5 +1,12 @@
 import { KeychainKeyTypes } from "hive-keychain-commons";
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 import { Signer } from "./signer/signer.entity";
 
 @Entity({ name: "signature-request" })
@@ -24,6 +31,19 @@ export class SignatureRequest {
 
   @Column()
   broadcasted?: boolean;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+  })
+  createdAt?: Date;
+
+  @UpdateDateColumn({
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP(6)",
+    onUpdate: "CURRENT_TIMESTAMP(6)",
+  })
+  updatedAt?: Date;
 
   @OneToMany(() => Signer, (signer) => signer.signatureRequest, {
     cascade: true,
